d21f4a98e8ef54faf9641c596a73b42e941980c2
Jasper_Son <32458465+SeokjinSon@users.noreply.github.com>
2021-04-16 20:30:15 +0900

    Update Detect_TuttleNeck.py
    Complete Simple Project

--- Detect_TuttleNeck.py
+++ Detect_TuttleNeck.py
@@ -1,28 +1,29 @@
 import cv2
 import tensorflow.keras
 import numpy as np
-import beepy
 import ctypes
 from PIL import Image, ImageOps
 
-MODEL_FILE_NAME = './.opencv-project2/datas/keras_model.h5'
+# Google Teachable Machine 으로 학습한 keras_model.h5의 경로
+MODEL_FILE_NAME = 'Input your keras_model.h5 Path'
 
+# 이미지 전처리
 def preprocessing(frame):
     size = (224, 224)
     frame_resized = cv2.resize(frame, size, interpolation=cv2.INTER_AREA)
     frame_normalized = (frame_resized.astype(np.float32) / 127.0) - 1
     frame_reshaped = frame_normalized.reshape((1, 224, 224, 3))
-
+    
     return frame_reshaped
 
-
+# 간단한 다이얼로그 창 표시
 def callDialog():
     msg = ctypes.windll.user32.MessageBoxW(None, "거북목 조심!", "경고창", 0)
-
 
 if __name__ == "__main__":
     model = tensorflow.keras.models.load_model(MODEL_FILE_NAME)
 
+    # 디바이스 내 카메라 사용
     capture = cv2.VideoCapture(0)
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, 420)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 420)
@@ -32,20 +33,26 @@ if __name__ == "__main__":
         if not ret:
             print("Image Load Failed")
 
+        # 이미지 좌우 반전
         frame_fliped = cv2.flip(frame, 1)
+        
+        cv2.imshow("Detecting Tuttle Neck", frame_fliped)
 
-        cv2.imshow("거북목 탐지 프로그램", frame_fliped)
-
+        # 'q' 버튼을 누르면 종료
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
 
+        # 데이터 전처리
         preprocessed = preprocessing(frame_fliped)
+        
+        # 전처리된 데이터를 통한 예측
         prediction = model.predict(preprocessed)
 
+        # prediction[0, 0] : no tuttle neck, prediction[0, 1] : tuttle neck
         if prediction[0, 0] < prediction[0, 1]:
             print("거북목 주의!")
-            callDialog()
             print(prediction)
+            callDialog()
             
         else:
             print("정상 상태")
